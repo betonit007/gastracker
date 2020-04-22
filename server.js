19230faed6d2b81b401fcff53eb93558ec049763
betonit007 <timnagorski@hotmail.com>
2020-04-22 10:18:37 -0400

    server.js settings

--- server.js
+++ server.js
@@ -1,31 +1,27 @@
 const express = require('express');
-const fileUpload = require('express-fileupload')
-const connectDB = require('./config/db')
-const path = require('path')
+const fileUpload = require('express-fileupload');
+const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
-connectDB()
+connectDB();
 
-app.use(express.json({ extended: false }))
-app.use(fileUpload())
+app.use(express.json({ extended: false }));
+app.use(fileUpload());
 
 app.use(`/api/users`, require(`./routes/api/users`));
 app.use(`/api/readings`, require(`./routes/api/readings`));
 app.use(`/api/auth`, require(`./routes/api/auth`));
 
-
-
-app.get('*', function(_, res) {
-  res.sendFile(path.join(__dirname, './client/build/index.html'), function(err) {
-    if (err) {
-      res.status(500).send(err)
-    }
-  })
-})
+//catch all route for production!!!!!!
+app.use(express.static('client/build'));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+});
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
 
 //GOOGLE_APPLICATION_CREDENTIALS="C:\Users\timna\dev\googleCloud\vision\resources\fuel.json"
