13648164a61b95cbe35d6db8ffc41e363f4604b0
betonit007 <timnagorski@hotmail.com>
2020-04-22 08:59:13 -0400

    predeploy

--- routes/api/resources/formatVisionResponse.js
+++ routes/api/resources/formatVisionResponse.js
@@ -1,24 +1,34 @@
  module.exports = reading => {
 
-    for (let i = 0; i <= 3; i++) {
-      if (reading[i].indexOf('Sheetz') > -1 || reading[i].indexOf('SHEETZ') > -1) {
+    
+      if (reading[0].indexOf('Sheetz') > -1 || reading[0].indexOf('SHEETZ') > -1) {
         
         const finalResults = {
-          store: reading[0],
+          store: 'Sheetz',
           street: reading[2],
           gallons: parseFloat(reading[10]).toFixed(2),
           total: parseFloat(reading[11].slice(1)).toFixed(2),
           state: reading[4],
           city: reading[3]
         }
-        console.log(finalResults)
+        
         return finalResults
       }
-      if (reading[i].indexOf('Shell') > -1 || reading[i].indexOf('SHELL') > -1) {
-        console.log('shell')
+      if (reading[0].indexOf('Shell') > -1 || reading[0].indexOf('SHELL') > -1) {
+        
+        const finalResults = {
+          store: "Shell",
+          street: reading[3],
+          gallons: parseFloat(reading[13]).toFixed(2),
+          total: parseFloat(reading[19].slice(1)).toFixed(2),
+          state: reading[5],
+          city: reading[4]
+        }
+       
+        return finalResults
       }
       if (reading[i].indexOf('Dans') > -1 || reading[i].indexOf('DANS') > -1) {
         console.log('shell')
       }
     }
-  }
+  
