474cc3483371a23af744d0f82b590e215ce65c61
betonit007 <timnagorski@hotmail.com>
2020-04-22 09:57:21 -0400

    try again

--- server.js
+++ server.js
@@ -14,13 +14,13 @@ app.use(`/api/users`, require(`./routes/api/users`));
 app.use(`/api/readings`, require(`./routes/api/readings`));
 app.use(`/api/auth`, require(`./routes/api/auth`));
 
-if (process.env.NODE_ENV === 'production') {
-    //catch all route for production!!!!!!
-    app.use(express.static('client/build'))
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-    })
-}
+
+//catch all route for production!!!!!!
+app.use(express.static('client/build'))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+})
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
