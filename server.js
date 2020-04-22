db93fce31aabfa042b19e2eb4a80958c4bc6f3d0
betonit007 <timnagorski@hotmail.com>
2020-04-22 12:52:39 -0400

    trying to add google auth creds

--- server.js
+++ server.js
@@ -19,6 +19,19 @@ if (process.env.NODE_ENV) {
     app.use(express.static('client/build'));
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+
+    process.env.GOOGLE_APPLICATION_CREDENTIALS = {
+        type: process.env.GOOGLE_TYPE,
+        project_id: process.env.GOOGLE_PROJECT_ID,
+        private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+        private_key: process.env.GOOGLE_PRIVATE_KEY,
+        client_email: process.env.GOOGLE_CLIENT_EMAIL,
+        client_id: process.env.GOOGLE_CLIENT_ID,
+        auth_uri: process.env.GOOGLE_AUTH_URI,
+        token_uri: process.env.GOOGLE_TOKEN_URI,
+        auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509,
+        client_x509_cert_url: process.env.GOOGLE_CLIENT_X509  
+    }
     });
 }
 
@@ -26,4 +39,4 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
 
-//GOOGLE_APPLICATION_CREDENTIALS="C:\Users\timna\dev\googleCloud\vision\resources\fuel.json"
+process.envGOOGLE_APPLICATION_CREDENTIALS="C:\Users\timna\dev\googleCloud\vision\resources\fuel.json"
