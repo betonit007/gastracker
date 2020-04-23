6cc460c8a5e772564d066f9c74948c230d7c9be0
betonit007 <timnagorski@hotmail.com>
2020-04-23 13:14:20 -0400

    preinstall changes

--- gcpSetup.js
+++ gcpSetup.js
@@ -1,3 +1,3 @@
 const fs = require('fs');
 
-fs.writeFile(process.env.GCP_KEY_FILE, process.env.GCP_CRED, (err) => {});
+fs.writeFile(process.env.GCP_KEY_FILE, process.env.GOOGLE_APPLICATION_CREDENTIALS, (err) => {});
